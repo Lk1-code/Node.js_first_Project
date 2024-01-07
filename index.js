20c5e80fa03a8dd3a1a068044b3187138f31b736
Lucas Mariano Da Silva <lucasm1593@gmail.com>
2024-01-06 23:59:17 -0300

    feat

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////////////////////
@@ -42,7 +44,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, {lower:true}))
 
+dataObj.forEach((el,i) =>el['slug'] = slugs[i]);
 
 const server = http.createServer((req, res) => {
    
@@ -60,9 +64,12 @@ const server = http.createServer((req, res) => {
     //Product page    
     }else if(pathname === '/product'){
         res.writeHead(200, {'Content-type' : 'text/html'});
-        const product = dataObj[query.id];
+       
+        const slugName = dataObj.map(el => el.slug === query.id)
+        const id = slugName.indexOf(true);
+        const product = dataObj[id];
         const output = replaceTemplate(tempProduct, product);
-        res.end(output);
+    res.end(output);
     //API    
     }else if(pathname == '/api'){
             res.writeHead(200, {'Content-type' : 'application/json'});
@@ -76,6 +83,7 @@ const server = http.createServer((req, res) => {
         });
         res.end('<h1> Page not found! </h1>');
     }
+    
 });
 server.listen(8000, '127.0.0.1', ()=>{
     console.log('Linstening to request on port 8000');
